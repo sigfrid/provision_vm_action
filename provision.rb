daced9b6ccd6d7e2f4f3251fb638be1f59f1d746
Sigfrid Dusci <sig@inforlife.ch>
2025-01-04 14:26:22 +0100

    Update provision.rb

--- provision.rb
+++ provision.rb
@@ -4,7 +4,11 @@ require 'dotenv/load'
 require "net/ssh"
 require "erb"
 
-Net::SSH.start(ENV["SSH_HOST"], ENV["SSH_USER"], keys: [], key_data: [File.read(ENV["SSH_KEY_PATH"])], keys_only: true) do |ssh|
-  puts ssh.exec!(ERB.new(File.read("./scripts/echo")).result)
+Net::SSH.start(ENV["SSH_HOST"],
+               ENV["SSH_USER"],
+               port: ENV["SSH_PORT"],
+               keys: [],
+               key_data: [File.read(ENV["SSH_KEY_PATH"])],
+               keys_only: true) do |ssh|
+  puts ssh.exec!(ERB.new(File.read("./scripts/harden_ssh")).result)
 end
-
