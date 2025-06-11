ed1cbee05f494c64ad1581773e2793a95a950f1e
Sigfrid Dusci <sigfrid@users.noreply.github.com>
2025-06-11 16:45:21 +0200

    Update run.rb

--- run.rb
+++ run.rb
@@ -4,13 +4,11 @@ require 'dotenv/load'
 require "net/ssh"
 require "erb"
 
-puts File.read(ENV["SSH_KEY_PATH"])
-
 SSH = Net::SSH.start(ENV["SSH_HOST"],
                ENV["SSH_USER"],
                port: ENV["SSH_PORT"],
                keys: [],
-               key_data: [File.read(ENV["SSH_KEY_PATH"])],
+               key_data: [ENV["SSH_KEY"]],
                keys_only: true)
 
 puts SSH.exec!(ERB.new(File.read("./scripts/00_echo")).result)
